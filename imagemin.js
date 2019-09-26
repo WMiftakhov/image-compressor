37cbdbbd3a306c9af9e1e145c1b0a0fa96575ef3
WMiftakhov <7978593@gmail.com>
2019-09-26 08:13:04 +0500

    commit for compressor app

---
+++ imagemin.js
@@ -0,0 +1,35 @@
+const imagemin = require("imagemin");
+const PNGImages = "assets/images/*.png";
+const JPEGImages = "assets/images/*.jpg";
+const output = "build/images";
+
+const imageminMozjpeg = require("imagemin-mozjpeg");
+const optimiseJPEGImages = () =>
+  imagemin([JPEGImages], {
+    destination: output,
+    plugins: [imageminMozjpeg({ quality: 70 })]
+  });
+
+const imageminPngquant = require('imagemin-pngquant');
+const optimisePNGImages = () =>
+  imagemin([PNGImages], {
+    destination: output,
+    plugins: [imageminPngquant({ quality: [0.65, 0.80] })]
+  });
+optimiseJPEGImages()
+    .then(() => optimisePNGImages())
+    .catch(error => console.log(error));
+
+    
+// const imagemin = require('imagemin');
+// const imageminMozjpeg = require('imagemin-mozjpeg');
+// (async () => {
+//     await imagemin(['assets/images/*.jpg'], 'build/images', {
+//         use: [
+//             imageminMozjpeg()
+//         ]
+//     });
+
+//     console.log('Images optimized');
+// })();
+
